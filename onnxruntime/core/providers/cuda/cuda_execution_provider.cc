7290f2b8edf337789e9d84038f409a80271c6a16
Weixing Zhang <Weixing.Zhang@microsoft.com>
2020-09-25 11:03:32 -0700

    rename CUDA/CUDA_PINNED to GPU/GPU_PINNED to share the code between Nvida GPU and AMD GPU

--- onnxruntime/core/providers/cuda/cuda_execution_provider.cc
+++ onnxruntime/core/providers/cuda/cuda_execution_provider.cc
@@ -71,7 +71,7 @@ CUDAExecutionProvider::PerThreadContext::PerThreadContext(OrtDevice::DeviceId de
 
   AllocatorCreationInfo default_memory_info(
       [](OrtDevice::DeviceId id) {
-        return onnxruntime::make_unique<CUDAAllocator>(id, CUDA);
+        return onnxruntime::make_unique<CUDAAllocator>(id, GPU);
       },
       device_id,
       true,
@@ -2040,7 +2040,7 @@ void CUDAExecutionProvider::RegisterAllocator(std::shared_ptr<AllocatorManager> 
   if (nullptr == cuda_pinned_alloc) {
     AllocatorCreationInfo pinned_memory_info(
         [](OrtDevice::DeviceId device_id) {
-          return onnxruntime::make_unique<CUDAPinnedAllocator>(device_id, CUDA_PINNED);
+          return onnxruntime::make_unique<CUDAPinnedAllocator>(device_id, GPU_PINNED);
         },
         DEFAULT_CPU_ALLOCATOR_DEVICE_ID);
 
